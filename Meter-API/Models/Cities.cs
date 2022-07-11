b1977a5af87582ee45a34ae94118bf1bf42b8809
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-07-11 15:57:23 +0530

    added city model

---
+++ Meter-API/Models/Cities.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Meter_API.Models
+{
+    public class Cities
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private long id;
+
+        private string name;
+
+        private bool active;
+        
+        private DateTime createdDate;
+
+        private DateTime lastModifiedDate;
+    }
+}
